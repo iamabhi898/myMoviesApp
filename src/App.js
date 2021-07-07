619a9d123958535f4721fd2ff0e6b99e48c1e5b7
Abhishek Tripathi <abhishek830318@gmail.com>
2021-07-07 21:05:36 +0530

    added the moviedb api

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 // import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  Button,
-} from 'react-native';
+import {StatusBar, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import config from '../config';
 
 import TabsNavigator from './navigation/TabsNavigator';
+// import API_KEY from '../.env';
 
 const App = () => {
+  // Theme - Coloring
   const [isDarkMode, setIsDarkMode] = React.useState(false);
-
   const bkgStyle = {
-    bkgColor: isDarkMode ? 'black' : 'white',
-    txtColor: isDarkMode ? 'white' : 'black',
+    bkgColor: isDarkMode ? '#050505' : '#EBEBEB',
+    secBkgColor: isDarkMode ? '#221F3B' : 'white',
+    txtColor: isDarkMode ? '#EBEBEB' : '#050505',
   };
+
+  const [moviesTitle, setMoviesTitle] = React.useState([]);
+
+  // Movies API
+  const fetchMoviesTitles = () => {
+    fetch(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${config.API_KEY}`,
+    )
+      .then(response => response.json())
+      .then(data => {
+        data.results.map(item =>
+          setMoviesTitle(state => [
+            ...state,
+            item.original_title || item.original_name,
+          ]),
+        );
+      });
+  };
+
+  React.useEffect(() => {
+    fetchMoviesTitles();
+  }, []);
 
   return (
     <NavigationContainer>
@@ -39,10 +48,17 @@ const App = () => {
         bkgStyle={bkgStyle}
         isDarkMode={isDarkMode}
         setIsDarkMode={setIsDarkMode}
+        moviesTitle={moviesTitle}
       />
     </NavigationContainer>
   );
 };
+
+// Color Palette
+// purple : #6F4A8E
+// gray : #221F3B
+// black : #050505
+// white : #EBEBEB
 
 const styles = StyleSheet.create({
   screen: {
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,10 +2,25 @@ import React from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
 
 const HomeScreen = props => {
-  const {bkgStyle, isDarkMode, setIsDarkMode} = props;
+  const {bkgStyle, isDarkMode, setIsDarkMode, moviesTitle} = props;
   return (
     <View style={{...styles.screen, backgroundColor: bkgStyle.bkgColor}}>
-      <Text style={{color: bkgStyle.txtColor}}>Home Screen</Text>
+      <View style={{marginBottom: 60}}>
+        <Text
+          style={{
+            color: bkgStyle.txtColor,
+            fontWeight: 'bold',
+            fontSize: 24,
+            marginBottom: 30,
+          }}>
+          Trending Movies and Shows
+        </Text>
+        {moviesTitle.map(item => (
+          <Text style={{color: bkgStyle.txtColor, fontWeight: 'bold'}}>
+            {item}
+          </Text>
+        ))}
+      </View>
       <Button
         title={isDarkMode ? 'Light Theme' : 'Dark Theme'}
         onPress={() => setIsDarkMode(!isDarkMode)}
