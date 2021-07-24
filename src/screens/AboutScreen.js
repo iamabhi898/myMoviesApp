506b05fb44b91ae34052fe8e6eb5c5db914873a3
Abhishek Tripathi <abhishek830318@gmail.com>
2021-07-24 18:40:16 +0530

    about screen done

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, ScrollView, StyleSheet, Linking} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const AboutScreen = props => {
   const {bkgStyle} = props;
   return (
     <View style={{...styles.screen, backgroundColor: bkgStyle.bkgColor}}>
-      <Text style={{color: bkgStyle.txtColor}}>About Screen</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.description}>
+          <Text style={{...styles.descriptionTxt, color: bkgStyle.txtColor}}>
+            This app is developed with the intend of learning, "My Movies" is a
+            personal project for portfolio of the developer. {'\n\n'}This app
+            uses TMDb API for the sole purpose of leaning app development and
+            developer do not intend to use this app for any kind of revenue.{' '}
+            {'\n\n'}
+            Permission is hereby granted, free of charge, to any person
+            obtaining a copy of this software, including without limitation the
+            rights to use, copy, modify, merge, publish, and distribute.{'\n\n'}
+            <Text style={{fontFamily: 'OpenSans-Italic'}}>July 2021</Text>
+          </Text>
+        </View>
+        <View style={styles.developerTitle}>
+          <Text style={{...styles.devName, color: bkgStyle.txtColor}}>
+            Abhishek Tripathi
+          </Text>
+          <View style={{flexDirection: 'row'}}>
+            <Ionicons
+              name={'logo-linkedin'}
+              color={bkgStyle.txtColor}
+              size={28}
+              style={{marginRight: 20}}
+              onPress={async () => {
+                await Linking.openURL(
+                  'https://www.linkedin.com/in/abhishek-tripathi-244a8b190/',
+                );
+              }}
+            />
+            <Ionicons
+              name={'logo-github'}
+              color={bkgStyle.txtColor}
+              size={28}
+              onPress={async () => {
+                await Linking.openURL('https://github.com/iamabhi898');
+              }}
+            />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -15,6 +56,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  description: {
+    marginVertical: 20,
+  },
+  descriptionTxt: {
+    paddingHorizontal: 20,
+    fontFamily: 'OpenSans-Regular',
+    fontSize: 15,
+  },
+  developerTitle: {
+    alignItems: 'center',
+  },
+  devName: {
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 18,
+    marginBottom: 10,
   },
 });
 
